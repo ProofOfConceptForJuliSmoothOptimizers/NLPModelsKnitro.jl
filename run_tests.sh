0335a21ef7b6916032be5ff84e5d6c31ad3c9ea6
JSOBot <69814847+JSOBot@users.noreply.github.com>
2021-06-30 14:33:15 -0400

    adding/updating file: run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-julia -E 'using Pkg(); Pkg.activate(joinpath("~", "tests", "NLPModelsKnitro")); Pkg.add("NLPModelKnitro"); Pkg.build("NLPModelKnitro"); Pkg.test("NLPModelKnitro")'
+julia -E 'using Pkg; Pkg.activate(joinpath("~", "tests", "NLPModelsKnitro")); Pkg.add("NLPModelKnitro"); Pkg.build("NLPModelKnitro"); Pkg.test("NLPModelKnitro")'
