94c9424db29e040e3b93db88e7f00b0071abfa18
Monssaf Toukal <tmonssaf@gmail.com>
2021-07-09 18:00:42 -0400

    fixing pipeline setup

--- run_tests.sh
+++ run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-julia -E 'using Pkg; Pkg.activate(joinpath("temp_env", "NLPModelsKnitro")); Pkg.add("NLPModelsKnitro"); Pkg.build("NLPModelsKnitro"); Pkg.test("NLPModelsKnitro")' &> test_results.txt
+julia -E 'using Pkg; Pkg.activate(joinpath("temp_env", "NLPModelsKnitro")); Pkg.develop(PackageSpec(url=joinpath("."))); Pkg.build("NLPModelsKnitro"); Pkg.test("NLPModelsKnitro")' &> test_results.txt
 
 # Create the gist and create comment on PR:
 julia test/send_gist_url.jl
