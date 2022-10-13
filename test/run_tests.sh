d2603d053706ff0e6f0e64bd23f98991859bf332
MonssafToukal <tmonssaf@gmail.com>
2022-10-13 18:08:17 -0400

    centralize jenkins test scripts

--- test/run_tests.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Specify the module to test (e.g "HSL")
-julia -E 'using Pkg; module_name = "NLPModelsKnitro"; Pkg.activate("test_env"); Pkg.develop(PackageSpec(url=joinpath("."))); Pkg.test(module_name)' &> test_results.txt
-
-# Create the gist and create comment on PR:
-julia test/send_gist_url.jl
